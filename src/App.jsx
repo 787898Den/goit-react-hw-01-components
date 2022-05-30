eff3e66397a00c4f5dd5b885c0a3ac96e40bc774
Denys Kochergin <dangerspace7@gmail.com>
2022-05-30 22:10:16 +0300

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
